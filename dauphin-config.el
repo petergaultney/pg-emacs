2f365aca6b00865a0031c067db5a4e00639dd008
Peter Gaultney <pgaultne@utk.edu>
2014-11-25 17:15:38 -0500

    fixed org bug

--- dauphin-config.el
+++ dauphin-config.el
@@ -55,9 +55,13 @@
 (global-set-key (kbd "C-c o a") 'org-agenda-list)
 (global-set-key (kbd "C-c o t") 'org-todo-list)
 
+(require 'org)
+(require 'org-install)
+
 (defun insert-org-timestamp-now ()
   "Does what it says."
   (interactive)
+  (require 'org-clock)
   (org-insert-time-stamp (org-current-time org-clock-rounding-minutes) 'with-hm))
 (define-key global-map (kbd "C-c o n") 'insert-org-timestamp-now)
 ;; (eval-after-load "org-mode"
