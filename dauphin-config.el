28fa8ee66b65e627341aaf73447fe8a97d4756ec
Peter Gaultney <pgaultne@utk.edu>
2015-02-26 23:09:06 -0500

    unfill-paragraph

--- dauphin-config.el
+++ dauphin-config.el
@@ -142,3 +142,11 @@
    )
   )
 
+(defun unfill-paragraph (&optional region)
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive (progn (barf-if-buffer-read-only) '(t)))
+  (let ((fill-column (point-max)))
+    (fill-paragraph nil region)))
+
+(define-key global-map "\M-Q" 'unfill-paragraph)
+
