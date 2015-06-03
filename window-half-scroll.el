0b1411daa7f1e687937b8987432f94879234ce25
Peter Gaultney <petergaultney@gmail.com>
2015-06-03 18:20:22 -0400

    oops. this was missing.

---
+++ window-half-scroll.el
@@ -0,0 +1,13 @@
+(defun window-half-height ()
+  (max 1 (/ (1- (window-height (selected-window))) 2)))
+
+(defun scroll-up-half ()
+  (interactive)
+  (scroll-up (window-half-height)))
+
+(defun scroll-down-half ()
+  (interactive)
+  (scroll-down (window-half-height)))
+
+(global-set-key [next] 'scroll-up-half)
+(global-set-key [prior] 'scroll-down-half)
