12699cff5de1213059895a53d71fb04875bb5355
Peter Gaultney <pgaultney@xoi.io>
2020-08-06 14:06:47 -0500

    even better colors for Python and other langs

--- dauphin-config-packages.el
+++ dauphin-config-packages.el
@@ -9,6 +9,8 @@
   (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
   ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
   (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+  (add-to-list 'package-archives
+			   '("melpa" . "http://melpa.milkbox.net/packages/") t)
   (when (< emacs-major-version 24)
     ;; For important compatibility libraries like cl-lib
     (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
@@ -30,6 +32,7 @@
     flycheck-flow
     material-theme
     better-defaults
+	color-identifiers-mode
     ))
 
 ;; Scans the list in myPackages
@@ -38,6 +41,8 @@
           (unless (package-installed-p package)
             (package-install package)))
       myPackages)
+
+(add-hook 'after-init-hook 'global-color-identifiers-mode)
 
 (eval-when-compile
   (require 'use-package))
