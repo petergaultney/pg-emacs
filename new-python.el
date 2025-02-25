f44e278ab4f5fd35a7f5aa5aa828739f6eac4216
Peter Gaultney <petergaultney@gmail.com>
2025-02-25 09:26:15 -0600

    fix dumb window margin issue with eglot...?

--- new-python.el
+++ new-python.el
@@ -20,24 +20,28 @@
 ;; elpaca seems unable to install eglot without eldoc 1.14... and won't resolve that on its own for whatever reason.
 
 (use-package
- eglot
- :ensure nil ;;(:host github :repo "joaotavora/eglot" :files (:defaults) :inherit nil)
- ;; :defer t
- :hook ((python-mode . eglot-ensure))
- :init (add-hook 'before-save-hook 'format-python-with-eglot-on-save)
- :config
- (setq-default eglot-workspace-configuration
-               '(:pylsp
-                 (:plugins
-                  (:pycodestyle (:enabled nil)
-                   :pyflakes (:enabled nil)
-                   :isort (:enabled t)
-                   :flake8 (:enabled nil)
-                   :pylsp-mypy (:enabled t)
-                   :black (:enabled t :line_length 105)))))
- (add-hook 'eglot--managed-mode-hook (lambda () (flymake-mode t)))
- (add-hook 'python-mode-hook 'highlight-numbers-mode)
- )
+  eglot
+  :ensure nil ;;(:host github :repo "joaotavora/eglot" :files (:defaults) :inherit nil)
+  ;; :defer t
+  :hook ((python-mode . eglot-ensure)
+          (python-mode . highlight-numbers-mode)
+          (eglot-managed-mode .
+            (lambda ()
+              (flymake-mode t)
+              (set-window-margins nil 0 nil)))  ;; i have no idea why eglot is adding this margin all of a sudden in 30.1
+          (before-save . format-python-with-eglot-on-save))
+
+  :init (add-hook 'before-save-hook 'format-python-with-eglot-on-save)
+  :config
+  (setq-default eglot-workspace-configuration
+    '(:pylsp
+       (:plugins
+         (:pycodestyle (:enabled nil)
+           :pyflakes (:enabled nil)
+           :isort (:enabled t)
+           :flake8 (:enabled nil)
+           :pylsp-mypy (:enabled t)
+           :black (:enabled t :line_length 105))))))
 
 ;; requires https://github.com/blahgeek/emacs-lsp-booster
 ;; to be built and on the PATH
